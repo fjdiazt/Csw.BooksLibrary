d8b933a39f7dfde167893b6f6f63b533fce490b6
Fred <Fred@DESKTOP-7HNQROR>
2016-09-25 17:41:27 -0400

    Add project files.

---
+++ Csw.Library/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Csw.Library
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters( GlobalFilterCollection filters )
+        {
+            filters.Add( new HandleErrorAttribute() );
+        }
+    }
+}
